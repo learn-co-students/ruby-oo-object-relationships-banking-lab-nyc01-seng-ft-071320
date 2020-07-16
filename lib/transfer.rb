d35eae950ce066680412c6e2a20c8b494c739388
cdemarti <codythomasdemartin@gmail.com>
2020-07-16 12:10:52 -0400

    Up to Transfer #deposit

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount   
+  
+  @@all = []
+  
+  
+  def initialize(sender, receiver, status="pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+    save 
+  end 
+
+  def self.all 
+    @@all
+  end    
+
+  def save
+    @@all << self 
+  end
+
+  def valid?
+    if self.sender.valid? == true && self.receiver.valid? == true
+      true
+    else  
+      false
+    end  
+  end
+
+  def execute_transaction
+    self.receiver.deposit(@amount)
+    binding.pry
+  end
+  
+  
+
+
+
+
 end
